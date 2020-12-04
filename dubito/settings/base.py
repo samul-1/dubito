fbac5a2dd51de73c08f8a273eebb25646773db2c
samul-1 <bsamuele00@gmail.com>
2020-12-04 21:55:28 +0100

    first deploy

--- dubito/settings/base.py
+++ dubito/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/game/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 ASGI_APPLICATION = "dubito.routing.application"
 
