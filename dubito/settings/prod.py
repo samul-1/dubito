734c03c8835fa2a8b92a06cf556c56b1a55ecab9
samul-1 <bsamuele00@gmail.com>
2021-01-05 12:58:04 +0100

    new version2

--- dubito/settings/prod.py
+++ dubito/settings/prod.py
@@ -29,3 +29,15 @@ LOGGING = {
         "django": {"handlers": ["console"], "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"), }
     },
 }
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
