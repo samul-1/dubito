1eae9ad90afa06f2925067b5aa58fbe561f06b52
samul-1 <bsamuele00@gmail.com>
2021-01-05 14:29:29 +0100

    new version2

--- dubito/settings/base.py
+++ dubito/settings/base.py
@@ -120,11 +120,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
-# os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
+# # os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dubitoapp/static/game')]
 
