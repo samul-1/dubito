528f999ca6b3c64b27c6e241b17b4934b23c3f36
samul-1 <bsamuele00@gmail.com>
2021-01-05 14:24:19 +0100

    new version2

--- dubito/settings/base.py
+++ dubito/settings/base.py
@@ -120,13 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/game/static/'
+STATIC_URL = '/static/'
 PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__))))))
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
 # os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dubitoapp/static/game')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dubitoapp/static/game')]
 
 ASGI_APPLICATION = "dubito.routing.application"
 
