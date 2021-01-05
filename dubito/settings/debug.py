3e8632bd05de9af455e1ce4dfca321d7762c2a2e
samul-1 <bsamuele00@gmail.com>
2021-01-05 13:41:31 +0100

    new version2

---
+++ dubito/settings/debug.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+print("project dir")
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
+print(os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+print("static root")
+print(os.path.join(PROJECT_DIR, 'staticfiles'))
+
+print("base dir")
+BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
+print(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))
+
+print("static root")
+print(os.path.join(BASE_DIR, 'staticfiles'))
