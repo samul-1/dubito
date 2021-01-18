cbed0f3d55cfc2d20b6af50611a99287b971231a
samul-1 <bsamuele00@gmail.com>
2021-01-18 12:37:49 +0100

    more transparent shadow in home btns

--- dubito/settings/base.py
+++ dubito/settings/base.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dubitoapp/static/game')]
 
-ASGI_APPLICATION = "dubito.routing.application"
+# ASGI_APPLICATION = "dubito.routing.application"
 
 # Channels
 ASGI_APPLICATION = 'dubito.routing.application'
