9442ba2841bf98469c5f854edca407e0a3f988d5
Samuele <bsamuele00@gmail.com>
2023-12-12 09:39:58 +0100

    deleted staticfiles

--- dubito/settings/prod.py
+++ dubito/settings/prod.py
@@ -34,11 +34,3 @@ LOGGING = {
         }
     },
 }
-
-
-# BASE_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
